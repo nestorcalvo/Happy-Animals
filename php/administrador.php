f412af6fc51362577da87a5013ff8046872e1fdd
nestorcalvo <nestor.calvo@udea.edu.co>
2020-06-20 15:21:25 -0500

    Cambio de algunos archivos de html a php para uso de DB

---
+++ php/administrador.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+<body>
+<ul>
+    <li><a href="voluntarios_add.php">Voluntarios</a></li>
+    <li><a href="solicitudes_add.php">Solicitudes adopción</a></li>
+    <li><a href="adopcion_acept.php">Solicitudes adopción</a></li>
+    <li style="float:right"><a class="active" href="../index.html" id="close_sesion">Cerrar sesion</a></li>
+</ul>
+
+
+</body>
+</html>
+<?php
+
---
+++ php/adopcion_acept.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+<body>
+<ul>
+    <li><a href="voluntarios_add.php">Voluntarios</a></li>
+    <li><a href="solicitudes_add.php">Solicitudes adopción</a></li>
+    <li><a class="active" href="adopcion_acept.php">Solicitudes adopción</a></li>
+    <li style="float:right"><a href="../index.html" id="close_sesion">Cerrar sesion</a></li>
+</ul>
+</body>
+</html>
+<?php
