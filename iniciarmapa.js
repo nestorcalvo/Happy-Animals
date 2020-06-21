e782b8f164da9ac61a51f9f8e8320d250d361ab5
victoriareyes11 <vickypanda22@hotmail.com>
2020-06-21 12:34:32 -0500

    posicion actual

--- iniciarmapa.js
+++ iniciarmapa.js
@@ -1,6 +1,10 @@
 window.onload = function(position){
 	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(indicarPosicion,error);
+        navigator.geolocation.getCurrentPosition(indicarPosicion,error,geo_options);
+        var geo_options={enableHightAccuracy: true, timeout:10000, maximage:1000};
+        
+        const watcher = navigator.geolocation.watchPosition(indicarPosicion);
+        setTimeout(() => {navigator.geolocation.clearWatch(watcher);}, 5000);
 	}
 }
 
@@ -8,8 +12,47 @@ var mapa;
 
 function indicarPosicion(position){
     var pos = document.getElementById("ubicacion");
-	pos.innerHTML = position.coords.latitude + ", "+position.coords.longitude;
+    var datos='';
+
+    datos+='Latitud: '+position.coords.latitude+'&nbsp&nbsp&nbsp';
+    datos+='Longitud: '+position.coords.longitude+'&nbsp&nbsp&nbsp';
+    datos+='Exactitud: '+position.coords.accuracy+' metros.<br>';
+
+	pos.innerHTML = datos;
     var coord = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    //var coord = new google.maps.LatLng(6.2633525, -75.5710481);
+   
     console.log(coord);
 
+    var obMapa = {
+        center: coord,
+        label: "A",
+        animation: google.maps.Animation.BOUNCE,
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		zoomControl: false,
+		fullscreenControl: false,
+		streetViewControl: false,
+		scaleControl: true
+	};
+    mapa = new google.maps.Map(document.getElementById("mapa"),obMapa);
+    
+    var opMarcaUser = {
+		position: coord,
+		title: "user",
+		//label: "A",
+		//icon: "user.png",
+		animation: google.maps.Animation.BOUNCE,
+		map: mapa
+	};
+    var marca = new google.maps.Marker(opMarcaUser);
+
+    
+    
+    
+
+}
+
+function error(){
+	alert("No se pudo obtener la posición.")
 }
