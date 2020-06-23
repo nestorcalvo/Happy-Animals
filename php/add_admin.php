c03e2d220ca597cb91a777fc190bc8c392aaad97
nestorcalvo <nestor.calvo@udea.edu.co>
2020-06-22 23:42:37 -0500

    Administrador: datos basicos y nuevo admin

---
+++ php/add_admin.php
@@ -0,0 +1,25 @@
+<?php
+    #Contectarse al servidor
+    $conectar = new mysqli('localhost', 'root', '', 'happy_animals');
+    $conectar->set_charset("utf8");
+    if($conectar->connect_error){
+        echo "No se pudo conectar con el server";
+    }
+    $user = $_POST['user_admin'];
+    $correo = $_POST['email_admin'];
+    $pass = $_POST['pass_admin'];
+
+    $sql = "INSERT INTO administradores(usuarioadmin, correo, clave) VALUES ('$user','$correo','$pass')";
+    $ejecutar = $conectar->query($sql);
+
+    if(!$ejecutar){
+        echo "Hubo error al ejectuar la cola";
+        echo "<br>";
+        echo $conectar->error;
+    }
+    else{
+        document.getElementById("add_admin").reset();
+        echo "Usuario registrado correctamente";
+
+    }
+?>
