32eb4dd5b8f09e10c2d6705b8c8d2219d27427d4
nestorcalvo <nestor.calvo@udea.edu.co>
2020-07-01 17:38:41 -0500

    Cambio de css

--- php/adopcion_acept.php
+++ php/adopcion_acept.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/estilo.css">
+    <link rel="stylesheet" href="../css/style_admin.css">
 </head>
 <body>
 <ul>
