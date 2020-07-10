5a6636d14550c43a3b405a3c8347db7bba07aa72
manuvlj <mrc78613@gmail.com>
2020-07-10 12:20:36 -0500

    Actualización de estilo

--- php/administrador.php
+++ php/administrador.php
@@ -6,13 +6,16 @@
     <link rel="stylesheet" href="../css/style_admin.css">
 </head>
 <body>
-    <ul>
-        <li><a href="../php/voluntarios_add.php">Voluntarios</a></li>
-        <li><a href="../php/solicitudes_add.php">Solicitudes para dar en adopción</a></li>
-        <li><a href="../php/adopcion_acept.php">Solicitudes adopción</a></li>
-        <li style="float:right"><a class="active" href="../index.html" id="close_sesion">Cerrar sesion</a></li>
 
-    </ul>
+    <nav>
+        <ul>
+            <li><a href="../php/voluntarios_add.php">Voluntarios</a></li>
+            <li><a href="../php/solicitudes_add.php">Solicitudes para dar en adopción</a></li>
+            <li><a href="../php/adopcion_acept.php">Solicitudes adopción</a></li>
+            <li style="float:right"><a class="active" href="../index.html" id="close_sesion">Cerrar sesion</a></li>
+
+        </ul>
+    </nav>
     <h2>Perfil</h2>
         <?php
         #Contectarse al servidor
@@ -32,10 +35,10 @@
         }
         else{
             while($rows = mysqli_fetch_assoc($ejecutar)){
-                echo "<h2>Usuario:</h2>";
+                echo "<h2>Usuario</h2>";
                 echo $rows['usuarioadmin'];
                 echo "<br>";
-                echo "<h2>Correo:</h2>";
+                echo "<h2>Correo</h2>";
                 echo $rows['correo'];
 
             }
@@ -43,23 +46,27 @@
 
         }
     ?>
-    <h2>Añadir administrador</h2>
-    <form action="add_admin.php" method="post" id="add_admin" target="frame_admin">
-        <label for="add_admin">Usuario: </label>
-        <input type="text" id="user_admin" name="user_admin" required>
-        <br>
-        <label for="add_admin">Email: </label>
-        <input type="email" id="email_admin" name="email_admin" required>
-        <br>
-        <label for="add_admin">Password: </label>
-        <input type="password" id="pass_admin" name="pass_admin" required>
-        <br>
-        <br>
-        <input type="submit" value="Registrar">
-    </form>
-    <iframe name="frame_admin" frameborder="0">
-    </iframe>
 
+    <hr>
+    <div class="aa">
+        <h2>Añadir administrador</h2>
+        <form action="add_admin.php" method="post" id="add_admin" target="frame_admin">
+            <label for="add_admin">Usuario </label><br>
+            <input type="text" id="user_admin" name="user_admin" required><br>
+            <br>
+            <label for="add_admin">Correo </label><br>
+            <input type="email" id="email_admin" name="email_admin" required><br>
+            <br>
+            <label for="add_admin">Contraseña </label><br>
+            <input type="password" id="pass_admin" name="pass_admin" required><br>
+            <br>
+            <input type="submit" value="Registrar">
+        </form>
+    </div>
+        <iframe name="frame_admin" frameborder="0">
+        </iframe>    
+    
+    <img src="../img/completelogo.png" alt="Happy Animals">
 </body>
 </html>
 
