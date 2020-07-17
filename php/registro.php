24961d02d344dcfff4f9b8f418b996b864143df3
manuvlj <mrc78613@gmail.com>
2020-07-17 18:31:00 -0500

    Cambios en estilo de página

--- php/registro.php
+++ php/registro.php
@@ -99,31 +99,31 @@ session_start();
     </div>
 
     <div class="container">
-	<h2 class="text-center">Nueva cuenta</h2>
+	<h2 class="text-center">New Account</h2>
 
 	<form method="POST" action="../php/controlSesionRegistro.php">
 		<?php
 		 if(isset($_GET['error']) && $_GET['error']==true ){
-            print("<h4>Error: El nombre de usuario ya existe </h4>");
+            print("<h4>Error: this username already exist. Please choose another one. </h4>");
             }
          if(isset($_GET['error2']) && $_GET['error2']==true ){
-                print("<h4>Error: Las contraseñas no coinciden </h4>");
+                print("<h4>Error: passwords do not match </h4>");
                 }
 		?>
 		
-        <input type="text" name="usuario" placeholder="Usuario"  required value="<?php if (isset($_SESSION['usuario'])){ echo $_SESSION['usuario']; } ?>"><br><br>
-        <input type="text" name="nombre" placeholder="Nombre" required value="<?php if (isset($_SESSION['nombre'])){ echo $_SESSION['nombre']; } ?>"><br><br>
-        <input type="text" name="apellidos" placeholder="Apellidos" required value="<?php if (isset($_SESSION['apellidos'])){ echo $_SESSION['apellidos']; } ?>"><br><br>
+        <input type="text" name="usuario" placeholder="User"  required value="<?php if (isset($_SESSION['usuario'])){ echo $_SESSION['usuario']; } ?>"><br><br>
+        <input type="text" name="nombre" placeholder="Name" required value="<?php if (isset($_SESSION['nombre'])){ echo $_SESSION['nombre']; } ?>"><br><br>
+        <input type="text" name="apellidos" placeholder="Lastname" required value="<?php if (isset($_SESSION['apellidos'])){ echo $_SESSION['apellidos']; } ?>"><br><br>
         <input type="text" name="email" placeholder="Email" required value="<?php if (isset($_SESSION['email'])){ echo $_SESSION['email']; } ?>"><br><br>
-        <input type="password" name="contrasena" placeholder="Contraseña" required><br><br>
-        <input type="password" name="recontrasena" placeholder="Confirmar Contraseña" required><br><br>
-        <input type="text" name="ciudad" placeholder="Ciudad" required value="<?php if (isset($_SESSION['ciudad'])){ echo $_SESSION['ciudad']; } ?>"><br><br>
-        <input type="text" name="direccion" placeholder="Dirección" required value="<?php if (isset($_SESSION['direccion'])){ echo $_SESSION['direccion']; } ?>"><br><br>
-        <label>Fecha de nacimiento</label><br>
-        <input type="date" name="fecha" placeholder="Fecha de nacimiento" required value="<?php if (isset($_SESSION['fecha'])){ echo $_SESSION['fecha']; } ?>"><br><br>
+        <input type="password" name="contrasena" placeholder="Password" required><br><br>
+        <input type="password" name="recontrasena" placeholder="Confirm Password" required><br><br>
+        <input type="text" name="ciudad" placeholder="City" required value="<?php if (isset($_SESSION['ciudad'])){ echo $_SESSION['ciudad']; } ?>"><br><br>
+        <input type="text" name="direccion" placeholder="Direction" required value="<?php if (isset($_SESSION['direccion'])){ echo $_SESSION['direccion']; } ?>"><br><br>
+        <label>Date of Birth</label><br>
+        <input type="date" name="fecha" placeholder="Date of Birth" required value="<?php if (isset($_SESSION['fecha'])){ echo $_SESSION['fecha']; } ?>"><br><br>
 
 	    
-        <input type="submit" name="registrar" value="Crear cuenta"><br><br>
+        <input type="submit" name="registrar" value="Create Account"><br><br>
 
         </form>
         </div>
